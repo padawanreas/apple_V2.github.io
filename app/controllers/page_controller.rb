db4e9abcf2378b3966ac40ca97c4faa36defe40e
padawanreas <rebeccaabiro@gmail.com>
2019-08-04 11:30:07 +0200

    second commit

---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,6 @@
+class PageController < ApplicationController
+  def home
+  end
+  def index
+  end
+end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "apple_crud_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "as_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
 
